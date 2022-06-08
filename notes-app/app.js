08416d2d93185122e6f76e1d9d5aac452d20c0f7
Suhas0512 <suhassa05@gmail.com>
2022-06-08 12:25:53 +0530

    testing changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-fs.writeFileSync("notes.txt", "My name is Suhas.");
-fs.appendFileSync("notes.txt", "I am 28 years old and live in bangalore");
+//fs.writeFileSync("notes.txt", "My name is Suhas.");
+fs.appendFileSync("notes.txt", "notes hello");
